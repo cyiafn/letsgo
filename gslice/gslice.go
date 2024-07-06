88119ec82b200052620c10a7d4ae6be65f1c4adc
Yifan Chen <dev@cyifan.dev>
2024-07-06 14:21:17 +0800

    added filter

--- gslice/gslice.go
+++ gslice/gslice.go
@@ -8,3 +8,15 @@ func Map[T, U any](s []T, f func(T) U) []U {
 	return r
 }
 
+func Filter[T any](s []T, f func(T) bool) []T {
+	r := make([]T, 0)
+	for _, v := range s {
+		v := v
+		if f(v) {
+			r = append(r, v)
+		}
+	}
+
+	return r
+}
+
